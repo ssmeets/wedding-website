98cc100741753f2a065f24fb8b1d923a971155c6
Sjoerd Smeets <ssmeets@mac.home>
2025-02-12 13:54:34 +0100

    Fixed client loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import "./globals.css";
 
